0e7156e3fec187223ea200ab43e2df2c66377425
BloodyTakao <bloodyfistyt@gmail.com>
2019-10-14 19:39:18 +0200

    Initialize Plugin Base

---
+++ BloodyTakao.BloodysServerGuard/PluginProperties.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nodsoft.YumeChan.PluginBase;
+
+namespace BloodyTakao.BloodysServerGuard
+{
+    class PluginProperties : IPlugin
+    {
+        public Version PluginVersion { get; } = typeof(PluginProperties).Assembly.GetName().Version;
+
+        public string PluginDisplayName { get; } = "Bloody's Server Guard";
+
+        public bool PluginStealth { get; } = false;
+
+        public bool PluginLoaded { get; internal set; }
+
+        public Task LoadPlugin()
+        {
+            PluginLoaded = true;
+            return Task.CompletedTask;
+        }
+
+        public Task UnloadPlugin()
+        {
+            PluginLoaded = false;
+            return Task.CompletedTask;
+        }
+    }
+}
